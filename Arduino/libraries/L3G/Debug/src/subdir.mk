c4ae913ea13b8510b52481d7af79d348f1a86df0
siddian <siddian@gmail.com>
2012-12-16 16:44:55 +0100

    Core libraries needed for the IMU made by polulu (MinIMU)

---
+++ Arduino/libraries/L3G/Debug/src/subdir.mk
@@ -0,0 +1,24 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../src/L3G.cpp 
+
+OBJS += \
+./src/L3G.o 
+
+CPP_DEPS += \
+./src/L3G.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: AVR C++ Compiler'
+	avr-g++ -I"/home/siddian/Dropbox/ArduinoWorkspace/arduino_core/src" -I"/home/siddian/Dropbox/ArduinoWorkspace/arduino_core/pins_arduino_328P_16MHz" -I"/home/siddian/Dropbox/ArduinoWorkspace/Wire/src" -I"/home/siddian/Dropbox/ArduinoWorkspace/Wire/utility" -Wall -g2 -gstabs -Os -ffunction-sections -fdata-sections -fno-exceptions -mmcu=atmega16 -DF_CPU=1000000UL -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
---
+++ Arduino/libraries/LSM303/328P_16MHz/src/LSM303.d
@@ -0,0 +1,39 @@
+src/LSM303.d src/LSM303.o: ../src/LSM303.cpp ../src/LSM303.h \
+ /home/siddian/Dropbox/ArduinoWorkspace/arduino_core/src/Arduino.h \
+ /home/siddian/Dropbox/ArduinoWorkspace/arduino_core/src/binary.h \
+ /home/siddian/Dropbox/ArduinoWorkspace/arduino_core/src/WCharacter.h \
+ /home/siddian/Dropbox/ArduinoWorkspace/arduino_core/src/WString.h \
+ /home/siddian/Dropbox/ArduinoWorkspace/arduino_core/src/HardwareSerial.h \
+ /home/siddian/Dropbox/ArduinoWorkspace/arduino_core/src/Stream.h \
+ /home/siddian/Dropbox/ArduinoWorkspace/arduino_core/src/Print.h \
+ /home/siddian/Dropbox/ArduinoWorkspace/arduino_core/src/Printable.h \
+ /home/siddian/Dropbox/ArduinoWorkspace/arduino_core/src/new.h \
+ /home/siddian/Dropbox/ArduinoWorkspace/arduino_core/pins_arduino_328P_16MHz/pins_arduino.h \
+ /home/siddian/Dropbox/ArduinoWorkspace/Wire/src/Wire.h \
+ /home/siddian/Dropbox/ArduinoWorkspace/arduino_core/src/Stream.h
+
+../src/LSM303.h:
+
+/home/siddian/Dropbox/ArduinoWorkspace/arduino_core/src/Arduino.h:
+
+/home/siddian/Dropbox/ArduinoWorkspace/arduino_core/src/binary.h:
+
+/home/siddian/Dropbox/ArduinoWorkspace/arduino_core/src/WCharacter.h:
+
+/home/siddian/Dropbox/ArduinoWorkspace/arduino_core/src/WString.h:
+
+/home/siddian/Dropbox/ArduinoWorkspace/arduino_core/src/HardwareSerial.h:
+
+/home/siddian/Dropbox/ArduinoWorkspace/arduino_core/src/Stream.h:
+
+/home/siddian/Dropbox/ArduinoWorkspace/arduino_core/src/Print.h:
+
+/home/siddian/Dropbox/ArduinoWorkspace/arduino_core/src/Printable.h:
+
+/home/siddian/Dropbox/ArduinoWorkspace/arduino_core/src/new.h:
+
+/home/siddian/Dropbox/ArduinoWorkspace/arduino_core/pins_arduino_328P_16MHz/pins_arduino.h:
+
+/home/siddian/Dropbox/ArduinoWorkspace/Wire/src/Wire.h:
+
+/home/siddian/Dropbox/ArduinoWorkspace/arduino_core/src/Stream.h:
