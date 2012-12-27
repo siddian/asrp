17a8629a9319a61e682e504c68722e1fe314cef9
siddian <siddian@gmail.com>
2012-12-28 00:09:36 +0100

    messing with stagings...

--- RaspberryPi/workspace/e-paper/src/Epaper.cpp
+++ RaspberryPi/workspace/e-paper/src/Epaper.cpp
@@ -61,6 +61,7 @@ Epaper::~Epaper() {
 }
 
 void Epaper::waitBusy() {
+	delayMicroseconds(1);//maybe the busypin is not really working...
 	while (digitalRead(mBUSYPin) > 0) {
 		std::cout << "waiting ... " << millis() << std::endl;
 		delayMicroseconds(1);
@@ -422,19 +423,19 @@ void Epaper::updateImage(EpaperImage &newImage) {
 	//TODO: when is this used?
 //	double stagetime = getTempCompensation() * 630;//in [ms]
 
-//	std::cout << "stage 1" << std::endl;
-//	writeInvImage(mOldImage);
-//	for (unsigned x = 0; x < ResX; x++) {
-//		for (unsigned y = 0; y < ResY; y++) {
-//			uint8_t pixel = mOldImage.getPixel(x,y);
-//			if (pixel == Px_Black || pixel == Px_NC) {
-//				mWhiteImage.setPixel(x,y,Px_NC);
-//			}
-//			if (pixel == Px_White) {
-//				mWhiteImage.setPixel(x,y,Px_White);
-//			}
-//		}
-//	}
+	std::cout << "stage 1" << std::endl;
+	writeInvImage(mOldImage);
+	for (unsigned x = 0; x < ResX; x++) {
+		for (unsigned y = 0; y < ResY; y++) {
+			uint8_t pixel = mOldImage.getPixel(x,y);
+			if (pixel == Px_Black || pixel == Px_NC) {
+				mWhiteImage.setPixel(x,y,Px_NC);
+			}
+			if (pixel == Px_White) {
+				mWhiteImage.setPixel(x,y,Px_White);
+			}
+		}
+	}
 	std::cout << "stage 2" << std::endl;
 	writeImage(mWhiteImage);
 	std::cout << "stage 3" << std::endl;
